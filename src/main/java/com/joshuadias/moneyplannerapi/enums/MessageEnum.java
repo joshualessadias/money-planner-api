7c1b009f934b70177f63855b4e7fcc4723c1eef2
joshua <joshualessadias@gmail.com>
2024-03-28 23:50:57 -0300

    adding deletion Outcome messages

--- src/main/java/com/joshuadias/moneyplannerapi/enums/MessageEnum.java
+++ src/main/java/com/joshuadias/moneyplannerapi/enums/MessageEnum.java
@@ -18,7 +18,9 @@ public enum MessageEnum {
     OUTCOME_UPDATED_WITH_ID("message.outcome.updated.with.id"),
     OUTCOME_FINDING_BY_ID("message.outcome.finding.by.id"),
     OUTCOME_FINDING_ALL_PAGEABLE("message.outcome.finding.all.pageable"),
-    OUTCOME_FOUND_ALL_PAGEABLE("message.outcome.found.all.pageable"),;
+    OUTCOME_FOUND_ALL_PAGEABLE("message.outcome.found.all.pageable"),
+    OUTCOME_DELETING_WITH_ID("message.outcome.deleting.with.id"),
+    OUTCOME_DELETED_WITH_ID("message.outcome.deleted.with.id"),;
 
     private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
     private final String description;
