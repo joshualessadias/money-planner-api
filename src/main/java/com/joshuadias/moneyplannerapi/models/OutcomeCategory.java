a1b7bd751458a35b1749ed6602c0aa36521b7bc0
joshua <joshualessadias@gmail.com>
2024-03-28 23:46:38 -0300

    adding correct annotations to OutcomeCategory model

--- src/main/java/com/joshuadias/moneyplannerapi/models/OutcomeCategory.java
+++ src/main/java/com/joshuadias/moneyplannerapi/models/OutcomeCategory.java
@@ -1,6 +1,8 @@
 package com.joshuadias.moneyplannerapi.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,12 +12,12 @@ import lombok.Setter;
 @Entity
 @Table(name = "outcome_category")
 public class OutcomeCategory extends BaseModel {
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
 
-    @Column(name = "description")
+    @Column(name = "description", length = 255)
     private String description;
 
-    @Column(name = "code")
+    @Column(name = "code", length = 50, nullable = false, unique = true)
     private String code;
 }
