8270f19419d446aba33961faddc1b2e981758a5e
joshua <joshualessadias@gmail.com>
2024-04-03 15:09:22 -0300

    implementing create OutcomeCategory use case

--- src/main/java/com/joshuadias/moneyplannerapi/services/OutcomeCategoryService.java
+++ src/main/java/com/joshuadias/moneyplannerapi/services/OutcomeCategoryService.java
@@ -1,9 +1,12 @@
 package com.joshuadias.moneyplannerapi.services;
 
+import com.joshuadias.moneyplannerapi.dto.requests.OutcomeCategoryRequestDTO;
 import com.joshuadias.moneyplannerapi.enums.MessageEnum;
+import com.joshuadias.moneyplannerapi.exceptions.BadRequestException;
 import com.joshuadias.moneyplannerapi.exceptions.NotFoundException;
 import com.joshuadias.moneyplannerapi.models.OutcomeCategory;
 import com.joshuadias.moneyplannerapi.repositories.OutcomeCategoryRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,28 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OutcomeCategoryService
         extends AbstractServiceRepository<OutcomeCategoryRepository, OutcomeCategory, Long> {
+
+    private OutcomeCategory buildOutcomeCategoryFromRequest(OutcomeCategoryRequestDTO outcomeCategoryRequestDto) {
+        var outcomeCategory = new OutcomeCategory();
+        outcomeCategory.setName(outcomeCategoryRequestDto.getName());
+        outcomeCategory.setDescription(outcomeCategoryRequestDto.getDescription());
+        return outcomeCategory;
+    }
+
+    private void checkIfNameExists(String name) {
+        if (repository.existsByName(name)) {
+            throw new BadRequestException(MessageEnum.OUTCOME_CATEGORY_ALREADY_EXISTS_WITH_NAME.getMessage(name));
+        }
+    }
+
+    @Transactional
+    public void create(OutcomeCategoryRequestDTO outcomeCategoryRequestDto) {
+        log.info(MessageEnum.OUTCOME_CATEGORY_CREATING.getMessage());
+        checkIfNameExists(outcomeCategoryRequestDto.getName());
+        var outcomeCategory = buildOutcomeCategoryFromRequest(outcomeCategoryRequestDto);
+        var savedOutcomeCategory = save(outcomeCategory);
+        log.info(MessageEnum.OUTCOME_CATEGORY_CREATED_WITH_ID.getMessage(String.valueOf(savedOutcomeCategory.getId())));
+    }
 
     public OutcomeCategory findByIdOrThrow(Long id) {
         return repository.findById(id).orElseThrow(() -> new NotFoundException(
