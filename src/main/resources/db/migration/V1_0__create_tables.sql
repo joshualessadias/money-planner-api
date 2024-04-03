d1038571c8ef19b1239d3cf0c9d417ff0aa90aba
joshua <joshualessadias@gmail.com>
2024-04-03 14:41:15 -0300

    removing code from OutcomeCategory

--- src/main/resources/db/migration/V1_0__create_tables.sql
+++ src/main/resources/db/migration/V1_0__create_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bank
 (
     id              bigint IDENTITY (1, 1) NOT NULL,
-    created_at      datetime               NOT NULL,
+    created_at      datetime     NOT NULL,
     last_updated_at datetime,
-    name            varchar(255)           NOT NULL,
+    name            varchar(255) NOT NULL,
     CONSTRAINT pk_bank PRIMARY KEY (id)
 )
 GO
@@ -11,11 +11,11 @@ GO
 CREATE TABLE outcome
 (
     id                bigint IDENTITY (1, 1) NOT NULL,
-    created_at        datetime               NOT NULL,
+    created_at        datetime       NOT NULL,
     last_updated_at   datetime,
-    description       varchar(255)           NOT NULL,
-    value             decimal(12, 2)         NOT NULL,
-    date              datetime               NOT NULL,
+    description       varchar(100)   NOT NULL,
+    value             decimal(12, 2) NOT NULL,
+    date              datetime       NOT NULL,
     category_id       bigint,
     payment_method_id bigint,
     bank_id           bigint,
@@ -26,11 +26,10 @@ GO
 CREATE TABLE outcome_category
 (
     id              bigint IDENTITY (1, 1) NOT NULL,
-    created_at      datetime               NOT NULL,
+    created_at      datetime    NOT NULL,
     last_updated_at datetime,
-    name            varchar(255)           NOT NULL,
+    name            varchar(50) NOT NULL,
     description     varchar(255),
-    code            varchar(255),
     CONSTRAINT pk_outcome_category PRIMARY KEY (id)
 )
 GO
@@ -38,13 +37,17 @@ GO
 CREATE TABLE payment_method
 (
     id              bigint IDENTITY (1, 1) NOT NULL,
-    created_at      datetime               NOT NULL,
+    created_at      datetime     NOT NULL,
     last_updated_at datetime,
-    name            varchar(255)           NOT NULL,
+    name            varchar(255) NOT NULL,
     description     varchar(255),
     code            varchar(255),
     CONSTRAINT pk_payment_method PRIMARY KEY (id)
 )
+GO
+
+ALTER TABLE outcome_category
+    ADD CONSTRAINT uc_outcome_category_name UNIQUE (name)
 GO
 
 ALTER TABLE outcome
