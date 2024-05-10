d578dbe9fc7800179bae5b2ebbe43ec45210b635
joshua <joshualessadias@gmail.com>
2024-05-10 13:51:57 -0300

    adding code and outcomes to bank

--- src/main/java/com/joshuadias/moneyplannerapi/models/Bank.java
+++ src/main/java/com/joshuadias/moneyplannerapi/models/Bank.java
@@ -3,11 +3,20 @@ package com.joshuadias.moneyplannerapi.models;
 import com.joshuadias.moneyplannerapi.base.BaseModel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
+import java.util.List;
 
 @Entity
 @Table(name = "bank")
 public class Bank extends BaseModel {
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 100, unique = true)
     private String name;
+
+    @Column(name = "code", length = 10, nullable = false, unique = true)
+    private String code;
+
+    @OneToMany(mappedBy = "bank")
+    private List<Outcome> outcomes;
 }
